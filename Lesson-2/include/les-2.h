d7abd3760fc3167318f2165ea857081a1345a5e0
Dmitry Zverev <dimazverev716@gmail.com>
2020-07-23 10:00:25 +0300

    Lesson 2 Task 2

--- Lesson-2/include/les-2.h
+++ Lesson-2/include/les-2.h
@@ -1,3 +1,6 @@
 #include <iostream>
 
 #define SUCCESS 0
+
+// Returns a remainder of a division
+int Remaider(int val1, int val2);
--- Lesson-2/src/les-2.cpp
+++ Lesson-2/src/les-2.cpp
@@ -2,13 +2,38 @@
 
 using namespace std;
 
+// Returns a remainder of a division 
+int Remainder(int val1, int val2)
+{
+    return val1 % val2;
+}
+
+// Prints integer to stdout
+void PrintV(int value)
+{
+    cout << value << endl;
+}
+
 int main()
 {
+    // Task 1
     short var1 = 45000;
     unsigned char var2 = 1400;
 
+    cout << "\tTask 1 - Types specifics" << endl;
     cout << "Variable 1: " << var1 << endl;
     cout << "Variable 2: " << var2 << endl;
+
+    // Task 2
+    int value1, value2;
+
+    cout << "\tTask 2 - Evaluation of a remainder" << endl;
+    cout << "Enter value 1: ";
+    cin >> value1;
+    cout << "Enter value 2: ";
+    cin >> value2;
+    cout << "The remainder is ";
+    PrintV(Remainder(value1, value2));
 
     return SUCCESS;
 }
