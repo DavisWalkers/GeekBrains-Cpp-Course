58b715cd1261e91b3d9614a180e7ff609fa90189
Dmitry Zverev <dimazverev716@gmail.com>
2020-07-25 16:32:26 +0300

    Fixed typo

--- Lesson-3/src/les-3.cpp
+++ Lesson-3/src/les-3.cpp
@@ -8,7 +8,7 @@ int _geek::AddNum(int val1, int val2)
 int main(int argc, char **argv)
 {
     // Task 1
-    std::cout << "Task 1 - Using namespace" << std::endl;
+    std::cout << "\tTask 1 - Using namespace" << std::endl;
     int val1 = 1;
     int val2 = 5;
     std::cout << "Simple function result: " << _geek::AddNum(val1, val2) << std::endl;
