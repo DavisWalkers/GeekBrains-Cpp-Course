d5b4de768c9bf786af52416323aa36a9881e1fc9
Dmitry Zverev <dimazverev716@gmail.com>
2020-07-25 16:56:28 +0300

    Completed Task 2

--- Lesson-3/src/les-3.cpp
+++ Lesson-3/src/les-3.cpp
@@ -1,6 +1,7 @@
 #include "les-3.h"
 
 using _geek::AddNum;
+using namespace std;
 
 int _geek::AddNum(int val1, int val2)
 {
@@ -10,9 +11,34 @@ int _geek::AddNum(int val1, int val2)
 int main(int argc, char **argv)
 {
     // Task 1
-    std::cout << "\tTask 1 - Using namespace" << std::endl;
+    cout << "\tTask 1 - Using namespace" << endl;
     int val1 = 1;
     int val2 = 5;
-    std::cout << "Simple function result: " << AddNum(val1, val2) << std::endl;
+    cout << "Simple function result: " << AddNum(val1, val2) << endl;
+
+    // Task 2
+    cout << "\tTask 2 - Conditionals" << endl;    
+    int input1, input2;
+    cout << "Enter a number: ";
+    cin >> input1;
+    
+    if (input1 > 5)
+    {
+        cout << "Enter another number: ";
+        cin >> input2;
+        if (input2 > 5 && input2 < 10)
+
+            input2 -= 5;
+        else
+            input2 = 5 - input2;
+
+        if (input2 == 0) 
+            cout << "The result number is zero" << endl;
+        else if (input2 > 0)    
+            cout << "The result number is greater than zero" << endl;
+        else 
+            cout << "The result number is less than zero" << endl;        
+    }
+
     return 0;
 }
