e5c55840c7551ed442225ff0cce3a9ee0dac4c2a
Dmitry Zverev <dimazverev716@gmail.com>
2020-07-25 16:34:09 +0300

    Improved task 1

--- Lesson-3/src/les-3.cpp
+++ Lesson-3/src/les-3.cpp
@@ -1,5 +1,7 @@
 #include "les-3.h"
 
+using _geek::AddNum;
+
 int _geek::AddNum(int val1, int val2)
 {
     return val1 + val2;
@@ -11,6 +13,6 @@ int main(int argc, char **argv)
     std::cout << "\tTask 1 - Using namespace" << std::endl;
     int val1 = 1;
     int val2 = 5;
-    std::cout << "Simple function result: " << _geek::AddNum(val1, val2) << std::endl;
+    std::cout << "Simple function result: " << AddNum(val1, val2) << std::endl;
     return 0;
 }
